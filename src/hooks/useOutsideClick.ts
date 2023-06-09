24d6a7b2a7dc71c64f3a0e954248fbb921205f7e
Rajat Verma <rajat311201@gmail.com>
2023-06-09 10:57:40 +0000

    added useOutsideClick  hook

---
+++ src/hooks/useOutsideClick.ts
@@ -0,0 +1,29 @@
+import { useEffect, useRef } from "react";
+
+type;
+
+export function useOutsideClick(handler, listenCapturing = true) {
+    const ref = useRef();
+
+    useEffect(
+        function () {
+            function handleClick(e) {
+                if (ref.current && !ref.current.contains(e.target)) {
+                    handler();
+                }
+            }
+
+            document.addEventListener("click", handleClick, listenCapturing);
+
+            return () =>
+                document.removeEventListener(
+                    "click",
+                    handleClick,
+                    listenCapturing
+                );
+        },
+        [handler, listenCapturing]
+    );
+
+    return ref;
+}
