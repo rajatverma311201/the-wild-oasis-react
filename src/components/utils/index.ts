714ce7a9618b3eb7893504eb577dbdff6ba3380e
Rajat Verma <rajat311201@gmail.com>
2023-06-14 09:47:44 +0000

    conflicts resolved

--- src/components/utils/index.ts
+++ src/components/utils/index.ts
@@ -1,2 +1,3 @@
+export { default as DarkModeToggle } from "./DarkModeToggle";
 export { default as Filter } from "./Filter";
 export { default as SortBy } from "./SortBy";
--- src/main.tsx
+++ src/main.tsx
@@ -2,10 +2,13 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import { GlobalStyles } from "@/styles";
+import DarkModeProvider from "./context/DarkModeContext.tsx";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
     <React.StrictMode>
         <GlobalStyles />
-        <App />
+        <DarkModeProvider>
+            <App />
+        </DarkModeProvider>
     </React.StrictMode>
 );
