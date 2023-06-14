a99e9260c58f32a4d8717e2fdc3613365a8ac84f
Rajat Verma <rajat311201@gmail.com>
2023-06-14 15:31:25 +0530

    changed logo in dark mode

--- src/components/ui/Logo.tsx
+++ src/components/ui/Logo.tsx
@@ -1,5 +1,5 @@
 import styled from "styled-components";
-// import { useDarkMode } from "../context/DarkModeContext";
+import { useDarkMode } from "../context/DarkModeContext";
 
 const StyledLogo = styled.div`
     text-align: center;
@@ -11,9 +11,7 @@ const Img = styled.img`
 `;
 
 function Logo() {
-    //   const { isDarkMode } = useDarkMode();
-
-    const isDarkMode = false;
+    const { isDarkMode } = useDarkMode();
     const src = isDarkMode ? "/logo-dark.png" : "/logo-light.png";
 
     return (
