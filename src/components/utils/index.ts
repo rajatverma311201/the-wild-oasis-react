bf998e15c4f04eebb2ec556d44a5971623a5882f
Rajat Verma <rajat311201@gmail.com>
2023-07-30 15:54:56 +0530

    pagination and other components

--- src/components/utils/index.ts
+++ src/components/utils/index.ts
@@ -2,3 +2,4 @@ export { default as DarkModeToggle } from "./DarkModeToggle";
 export { default as Filter } from "./Filter";
 export { default as SortBy } from "./SortBy";
 export { default as TableOperations } from "./TableOperations";
+export { default as Pagination } from "./Pagination";
