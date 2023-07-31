9736b6e847f79f75b91e1d7ba4ddf119e807ff80
Rajat Verma <rajat311201@gmail.com>
2023-08-01 00:10:24 +0530

    dashboard complete

--- src/features/dashboard/DashboardLayout.tsx
+++ src/features/dashboard/DashboardLayout.tsx
@@ -1,15 +1,15 @@
 import styled from "styled-components";
 
-import Statistics from "./Statistics";
-// import SalesChart from "./SalesChart";
-// import DurationChart from "./DurationChart";
-// import TodayActivity from "../check-in-out/TodayActivity";
-
 import { Spinner } from "@/components/ui";
 
 import { useRecentBookings, useRecentStays } from "@/hooks/bookings";
 import { useCabins } from "@/hooks/cabins";
+
+import Statistics from "./Statistics";
 import SalesChart from "./SalesChart";
+import DurationChart from "./DurationChart";
+
+import { TodayActivity } from "../check-in-out";
 
 const StyledDashboardLayout = styled.div`
     margin: 2.5rem auto;
@@ -34,8 +34,8 @@ function DashboardLayout() {
                 numDays={numDays}
                 cabinCount={cabins?.length}
             />
-            {/* <TodayActivity /> */}
-            {/* <DurationChart confirmedStays={confirmedStays} /> */}
+            <TodayActivity />
+            <DurationChart confirmedStays={confirmedStays} />
             <SalesChart bookings={bookings} numDays={numDays} />
         </StyledDashboardLayout>
     );
