3ad54a26b3b29ae9a7e30df384d76475ab773796
Rajat Verma <rajat311201@gmail.com>
2023-08-01 12:47:26 +0530

    suspense remove:

--- src/App.tsx
+++ src/App.tsx
@@ -4,97 +4,59 @@ import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import { QueryClientProvider, QueryClient } from "@tanstack/react-query";
 import { Toaster } from "react-hot-toast";
 
+import { AppLayout } from "@/components/layout";
+
 /* PAGE IMPORTS */
-
+import {
+    Account,
+    Booking,
+    Bookings,
+    Cabins,
+    Checkin,
+    Dashboard,
+    Login,
+    PageNotFound,
+    Settings,
+    Users,
+} from "@/pages";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
-import { Suspense, lazy } from "react";
-import styled from "styled-components";
-import { Logo, Spinner } from "@/components/ui";
-import { ProtectedRoute } from "@/features/authentication";
-
-const Account = lazy(() => import("@/pages/Account"));
-const Booking = lazy(() => import("@/pages/Booking"));
-const Bookings = lazy(() => import("@/pages/Bookings"));
-const Cabins = lazy(() => import("@/pages/Cabins"));
-const Checkin = lazy(() => import("@/pages/Checkin"));
-const Dashboard = lazy(() => import("@/pages/Dashboard"));
-
-const Settings = lazy(() => import("@/pages/Settings"));
-const Users = lazy(() => import("@/pages/Users"));
-const Login = lazy(() => import("@/pages/Login"));
-const PageNotFound = lazy(() => import("@/pages/PageNotFound"));
-const AppLayout = lazy(() => import("@/components/layout/AppLayout"));
+import { ProtectedRoute } from "./features/authentication";
 
 const queryClient = new QueryClient();
-
-const StyledSpinner = styled.div`
-    width: 100%;
-    height: 100vh;
-    background-color: var(--color-grey-50);
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    flex-direction: column;
-    gap: 2.5rem;
-`;
 
 function App() {
     return (
         <QueryClientProvider client={queryClient}>
             <ReactQueryDevtools initialIsOpen={false} />
-            <Suspense
-                fallback={
-                    <StyledSpinner>
-                        <Logo /> <Spinner />
-                    </StyledSpinner>
-                }
-            >
-                <BrowserRouter>
-                    <Routes>
+            <BrowserRouter>
+                <Routes>
+                    <Route
+                        element={
+                            <ProtectedRoute>
+                                <AppLayout />
+                            </ProtectedRoute>
+                        }
+                    >
+                        <Route index element={<Navigate to="/dashboard" />} />
+                        <Route path="/account" element={<Account />} />
+                        <Route path="/bookings" element={<Bookings />} />
                         <Route
-                            element={
-                                <ProtectedRoute>
-                                    <AppLayout />
-                                </ProtectedRoute>
-                            }
-                        >
-                            <>
-                                <Route
-                                    index
-                                    element={<Navigate to="/dashboard" />}
-                                />
-
-                                <Route path="/account" element={<Account />} />
-                                <Route
-                                    path="/bookings"
-                                    element={<Bookings />}
-                                />
-                                <Route
-                                    path="bookings/:bookingId"
-                                    element={<Booking />}
-                                />
-                                <Route
-                                    path="checkin/:bookingId"
-                                    element={<Checkin />}
-                                />
-                                <Route path="/cabins" element={<Cabins />} />
-                                <Route
-                                    path="/dashboard"
-                                    element={<Dashboard />}
-                                />
-                                <Route
-                                    path="/settings"
-                                    element={<Settings />}
-                                />
-                                <Route path="/users" element={<Users />} />
-                            </>
-                        </Route>
-
-                        <Route path="/login" element={<Login />} />
-                        <Route path="*" element={<PageNotFound />} />
-                    </Routes>
-                </BrowserRouter>
-            </Suspense>
+                            path="bookings/:bookingId"
+                            element={<Booking />}
+                        />
+                        <Route
+                            path="checkin/:bookingId"
+                            element={<Checkin />}
+                        />
+                        <Route path="/cabins" element={<Cabins />} />
+                        <Route path="/dashboard" element={<Dashboard />} />
+                        <Route path="/settings" element={<Settings />} />
+                        <Route path="/users" element={<Users />} />
+                    </Route>
+                    <Route path="/login" element={<Login />} />
+                    <Route path="*" element={<PageNotFound />} />
+                </Routes>
+            </BrowserRouter>
             <Toaster
                 position="top-center"
                 gutter={12}
@@ -120,3 +82,46 @@ function App() {
 }
 
 export default App;
+
+/*
+    <div>
+            <Heading as="h1">H1 Hello World</Heading>
+            <Heading as="h2">H2 Hello World</Heading>
+            <Heading as="h3">H3 Hello World</Heading>
+            <Heading as="h4">H4 Hello World</Heading>
+            </div>
+            <div>
+                <Center>
+                    <Button>Check In</Button>
+                </Center>
+            </div>
+            <div>
+                <Center>
+                    <Input placeholder="Hello" />
+                </Center>
+            </div>
+            <div>
+                <Row justify="even">
+                    <Button size="large">Check In</Button>
+                    <Button variant="secondary">Check In</Button>
+                </Row>
+            </div>
+            <div>
+                <Center>
+                    <Column align="center">
+                        <Button size="small">Check In</Button>
+                        <Button>Check In</Button>
+                        <Button size="large">Check In</Button>
+                    </Column>
+                </Center>
+            </div>
+            <div>
+                <Center>
+                    <Column align="center">
+                        <Button>Check In</Button>
+                        <Button variant="secondary">Check In</Button>
+                        <Button variant="danger">Check In</Button>
+                    </Column>
+                </Center>
+    </div>
+*/
